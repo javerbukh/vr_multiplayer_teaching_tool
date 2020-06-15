d8dd0b75372b0de832b19ecfbd898f207889f8c0
Jesse Averbukh <javerbukh@gmail.com>
2020-06-15 15:42:49 -0400

    Setting up multiplayer
    Setting up network and player features in order to test host to client and client to host interactions through a server.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject CubePrefab;
+    GameObject m_InstantiatedCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject m_InstantiatedCube = Instantiate(CubePrefab);
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            m_InstantiatedCube.transform.position = position;
+            Debug.Log("Space pressed!!");
+
+            NetworkServer.Spawn(m_InstantiatedCube);
+
+        }
+    }
+}
